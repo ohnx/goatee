37610d4083327699fb6574f699d7a866bdf4ee49
Mason X <masonx.01@gmail.com>
2017-01-31 18:12:25 +0000

    with hexdump

--- tests/runner.c
+++ tests/runner.c
@@ -15,6 +15,59 @@ void error(char *str) {
     exit(-1337);
 }
 
+void hexDump (char *desc, void *addr, int len) {
+    int i;
+    unsigned char buff[17];
+    unsigned char *pc = (unsigned char*)addr;
+
+    /* Output description if given. */
+    if (desc != NULL)
+        printf ("%s:\n", desc);
+
+    if (len == 0) {
+        printf("  ZERO LENGTH\n");
+        return;
+    }
+    if (len < 0) {
+        printf("  NEGATIVE LENGTH: %i\n",len);
+        return;
+    }
+
+    /* Process every byte in the data. */
+    for (i = 0; i < len; i++) {
+        /* Multiple of 16 means new line (with line offset). */
+
+        if ((i % 16) == 0) {
+            /* Just don't print ASCII for the zeroth line. */
+            if (i != 0)
+                printf ("  %s\n", buff);
+
+            /* Output the offset. */
+            printf ("  %04x ", i);
+        }
+
+        /* Now the hex code for the specific character. */
+        printf (" %02x", pc[i]);
+
+        /* And store a printable ASCII character for later. */
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    /* Pad out last line if not exactly 16 characters. */
+    while ((i % 16) != 0) {
+        printf ("   ");
+        i++;
+    }
+
+    /* And print the final ASCII bit. */
+    printf ("  %s\n", buff);
+}
+
+
 char *dumpFile(const char *filename) {
     char *buffer = 0;
     long length;
@@ -53,6 +106,8 @@ int main(int argc, char *argv[]) {
 
     /* that's all that's done so far... print out output */
     printf("goatee_gen out:\n%s", out);
+    
+    hexDump("goatee_gen out", string_to_sr(out), sizeof(string_real) + string_to_sr(out)->tot);
     
     /* generate the output */
     outFinal = goatee_run(NULL, out);
